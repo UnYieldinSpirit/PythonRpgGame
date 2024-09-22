f28e26baf7d9508dffa6d704ecf2f86b24a88245
UnYieldinSpirit <unyieldinspirit@gmail.com>
2024-09-22 00:24:08 -0400

    Began the creation of the game's main player mechanics. Also created a means to properly document the changes that are occurring to the game.

---
+++ Player.py
@@ -0,0 +1,10 @@
+class Player:
+    def __init__(self):
+        default = 50
+        self.max_health = default
+        self.shield = 0
+        self.stamina = default
+        self.stamina = default
+
+    def buff(self, stat, value):
+        pass
